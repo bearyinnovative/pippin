272c5b67224717ab331e62228f48d8ebc98387e8
hbc <bcxxxxxx@gmail.com>
2017-03-11 23:03:19 +0800

    fix: drop nuxt

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var Koa = require('koa')
-var app = new Koa()
-var Nuxt = require('nuxt')
-var Router = require('koa-router')
-var AV = require('leanengine')
+const Koa = require('koa')
+const app = new Koa()
+const Router = require('koa-router')
+const AV = require('leanengine')
+const serve = require('koa-static')
+const mount = require('koa-mount')
 
 AV.init({
   appId: process.env.LEANCLOUD_APP_ID || '{{appid}}',
@@ -10,37 +11,20 @@ AV.init({
   masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || '{{masterkey}}',
 })
 
-var config = require('./nuxt.config.js')
-config.dev = !(app.env === 'production')
-
-var nuxt = new Nuxt(config)
-
-// Build only in dev mode
-if (config.dev) {
-  nuxt.build()
-  .catch((error) => {
-    console.error(error) // eslint-disable-line no-console
-    process.exit(1)
-  })
-}
-
-app.use(AV.koa())
-
-var router = new Router()
-var apiRoute = require('./api/route')
-
-router.use('/api/v1', apiRoute.routes(), apiRoute.allowedMethods())
-
-app.use(router.routes())
-
 app.context.render = require('./api/response')
 app.context.bearychat = require('./api/bearychat')()
 app.context.models = require('./storage/lean')
 app.context.config = require('./pippin.config.js')
 
-app.use(async (ctx, next) => {
-  ctx.status = 200 // koa defaults to 404 when it sees that status is unset
-  await nuxt.render(ctx.req, ctx.res)
-})
+app.use(AV.koa())
+
+// api route
+const router = new Router()
+const apiRoute = require('./api/route')
+router.use('/api/v1', apiRoute.routes(), apiRoute.allowedMethods())
+app.use(router.routes())
+
+// static file
+app.use(mount('/s', serve(__dirname + '/s')))
 
 app.listen(process.env.LEANCLOUD_APP_PORT)
